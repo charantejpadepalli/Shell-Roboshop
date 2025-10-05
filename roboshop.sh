033085e4adb3627ece42d164c61cb08c06173c34
Charan <charan@gmail.com>
2025-10-05 13:54:46 +0530

    Shell-Roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -7,14 +7,14 @@ DOMAIN_NAME="devopspractice.shop"
 
 for instance in $@
 do
-    INSTANCE_ID=$( aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,tags=[{Key=Name,Value=$instance}]" --query 'Instance[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,tags=[{Key=Name,Value=$instance}]" --query 'Instance[0].InstanceId' --output text)
 
     #get private ip
     if [ $instance != "frontend" ]; then
-        IP=$( aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Instance[0].PrivateIpAddress' --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Instance[0].PrivateIpAddress' --output text)
         RECORD_NAME="$instance.$DOMAIN_NAME"
     else
-        IP=$( aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Instance[0].PublicIpAddress' --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Instance[0].PublicIpAddress' --output text)
         RECORD_NAME="$DOMAIN_NAME"
     fi
 
