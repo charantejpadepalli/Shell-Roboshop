9a6273d5f360cc387c0bce3d03c33c2545bbdd21
Charan <charan@gmail.com>
2025-10-11 15:37:40 +0530

    Shell-Roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -5,20 +5,21 @@ SG_ID="sg-007348acaafd7df1b" #insert sg id
 ZONE_ID="Z07634881IMZZFH5TBV0T" #insert zone id 
 DOMAIN_NAME="devopspractice.shop"
 
-for instance in $@
+for instance in $@ # mongodb redis mysql
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instance[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
-    #get private ip
+    # Get Private IP
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
-        RECORD_NAME="$instance.$DOMAIN_NAME"
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws86s.fun
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-        RECORD_NAME="$DOMAIN_NAME"
+        RECORD_NAME="$DOMAIN_NAME" # daws86s.fun
     fi
 
     echo "$instance: $IP"
+
 
 aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
